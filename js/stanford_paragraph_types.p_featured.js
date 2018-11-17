935fa243474196fe1677ec2520b5b07e917c8b3e
Greg Garvey <ggarvey@stanford.edu>
2018-11-16 19:34:39 -0800

    Modifications made due to SOE GTM conflict

--- js/stanford_paragraph_types.p_featured.js
+++ js/stanford_paragraph_types.p_featured.js
@@ -8,6 +8,7 @@
 
     attach: function (context, settings) {
 
+
       // Loop through each featured video fields and add a unique ID.
       $('.field-name-field-p-featured-video iframe', context).each(function (i, iframe) {
         // Make sure the video has an ID attribute.
@@ -15,11 +16,6 @@
           $(this).attr('id', 'featured-video-' + i);
         }
       });
-
-      // If there is a youtube player, add the player api script.
-      if ($('.field-name-field-p-featured-video iframe[src*="youtube"]', context).length) {
-        $.getScript('https://www.youtube.com/iframe_api');
-      }
 
       // If there is a vimeo video add the player api script.
       if ($('.field-name-field-p-featured-video iframe[src*="vimeo"]', context).length) {
@@ -46,56 +42,84 @@
           return;
         }
 
-        // On each of the video elements attach an onReady event and set the
-        // title for accessibility reasons.
-        $video.attr('onload', 'this.contentWindow.focus()')
-          .attr('title', Drupal.t('Video Player'));
+        // Get Video Id and Attributes
+        var $videoSource = $video.attr('src');
+        var $srcParts = $videoSource.split('/');
+        var $videoInfo  = $srcParts[4].split('?');
+        var $videoId = $videoInfo[0];
 
-        // Initialze the Youtube api.
-        new YT.Player($video.attr('id'), {
+        //Get video attributes
+        var $videoAttr = decodeURIComponent($videoInfo[1]);
+        $videoAttr = $videoAttr.replace(/&amp;/g, '&');
+
+        // Helper function to get paramaters from iframe attributes
+        function getUrlParameter(name) {
+          name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+          var regex = new RegExp('(&amp;|)' + name + '=([^&#]*)', 'i' );
+          var results = regex.exec($videoAttr);
+          return results === null ? '' : results[0].replace(/\+/g, ' ');
+        };
+
+        // Get the width of the video
+        $videoWidthParts = getUrlParameter('width');
+        $videoWidthParts = $videoWidthParts.split('=');
+        $videoWidth = $videoWidthParts[1];
+
+        // Get the height of the video
+        $videoHeightParts = getUrlParameter('height');
+        $videoHeightParts = $videoHeightParts.split('=');
+        $videoHeight = $videoHeightParts[1];
+
+        $video.parent().attr('id', 'featured-video-' + i);
+        $video.remove();
+
+        var player = new YT.Player('featured-video-' + i, {
+          height: $videoHeight,
+          width: $videoWidth,
+          videoId: $videoId,
           events: {
-            'onReady': onYoutubePlayerReady
-          }
+            'onReady': Drupal.behaviors.stanfordParagraphPFeatured.onYoutubePlayerReady
+          },
+          host: 'https://www.youtube.com'
         });
-        // Force the iframe to allow the api event listeners.
-        $video.attr('src', $video.attr('src') + '&enablejsapi=1');
       });
 
-
-      /**
-       * When the youtube video is ready to play, we'll add a play button over
-       * the image and add the click listener.
-       *
-       * @param event
-       */
-      function onYoutubePlayerReady(event) {
-        $fieldWrapper = $(event.target.a.closest('.field-name-field-p-featured-video'));
-        $imageOverlay = $fieldWrapper.siblings('.field-name-field-p-featured-image');
-
-        var $play = Drupal.behaviors.stanfordParagraphPFeatured.getPlayerButton(event.target.getVideoUrl());
-
-        // Add a click event to hide the featured image and play the video.
-        $play.click(function (e) {
-          // Mouse has eventPhase 3, keyboard has 2. we only add the mouse
-          // event so that a keyboard will navigate the user to the respective
-          // YouTube page.
-          if (e.eventPhase == 3) {
-            e.preventDefault();
-
-            // Dad is the featured image.
-            $dad = $(this).parent();
-            $dad.hide();
-
-            // Play and show the video.
-            $fieldWrapper.show();
-            event.target.playVideo();
-          }
-        });
-
-        // Prepend the play button to the DOM.
-        $imageOverlay.prepend($play);
-      }
     },
+
+    /**
+     * When the youtube video is ready to play, we'll add a play button over
+     * the image and add the click listener.
+     *
+     * @param event
+     */
+    onYoutubePlayerReady: function (event) {
+
+      $fieldWrapper = $(event.target.a.closest('.field-name-field-p-featured-video'));
+      $imageOverlay = $fieldWrapper.siblings('.field-name-field-p-featured-image');
+
+      var $play = Drupal.behaviors.stanfordParagraphPFeatured.getPlayerButton(event.target.getVideoUrl());
+      // Add a click event to hide the featured image and play the video.
+      $play.click(function (e) {
+        // Mouse has eventPhase 3, keyboard has 2. we only add the mouse
+        // event so that a keyboard will navigate the user to the respective
+        // YouTube page.
+        if (e.eventPhase == 3) {
+          e.preventDefault();
+
+          // Dad is the featured image.
+          $dad = $(this).parent();
+          $dad.hide();
+
+          // Play and show the video.
+          $fieldWrapper.show();
+          event.target.playVideo();
+        }
+      });
+
+      // Prepend the play button to the DOM.
+      $imageOverlay.prepend($play);
+    },
+
 
     /**
      * After vimeo API is loaded, add the play button and listener to iframes.
@@ -157,9 +181,25 @@
 })(jQuery);
 
 /**
- * Called automatically by YouTube's API.
+ * Custom Event Listener for an Event in SOE Google Tag Manager
+ * https://tagmanager.google.com
+ * Use swsdevelopers@gmail.com to login
+ * The GTM tag with the referenced event 'dpTrigger' is in: Listener YouTube Tag
+ * Code Section:
+ * // Create the event for Paragraph Videos
+ * var dpYTReady = document.createEvent('Event');
+ * // Define that the event name is 'dpTrigger'.
+ * dpYTReady.initEvent('dpTrigger', true, true);
+ * // Dispatch the event
+ * document.dispatchEvent(dpYTReady);
  */
-function onYouTubeIframeAPIReady() {
-  // After Youtube API is loaded, we can do what we need.
-  Drupal.behaviors.stanfordParagraphPFeatured.youTubeReady();
-}
+document.addEventListener('dpTrigger', function () {
+  function readyYoutube(){
+    if((typeof YT !== "undefined") && YT && YT.Player){
+      Drupal.behaviors.stanfordParagraphPFeatured.youTubeReady();
+    }else{
+      setTimeout(readyYoutube, 100);
+    }
+  }
+  readyYoutube();
+});
